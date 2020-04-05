2a73fefa9380cbf8ba95c69bb2203b75cec1cfe2
AaronMorgan582 <aaron.st.morgan@gmail.com>
2020-04-05 14:00:26 -0600

    Added components for the login screen.

--- Agario/ViewController/Client_and_GUI.Designer.cs
+++ Agario/ViewController/Client_and_GUI.Designer.cs
@@ -34,20 +34,84 @@ namespace ViewController
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "Agario";
 
-            this.Connect_Button = new System.Windows.Forms.Button();
-            this.Connect_Button.Location = new Point(50, 50);
-            this.Connect_Button.Size = new Size(100, 50);
-            this.Connect_Button.Name = "Connect Button";
-            this.Connect_Button.Text = "Connect";
+            this.connect_button = new Button();
+            this.connect_button.Location = new Point(300, 260);
+            this.connect_button.Size = new System.Drawing.Size(100, 50);
+            this.connect_button.Name = "Connect Button";
+            this.connect_button.Text = "Connect";
             //this.Connect_Button.Click += Connect;
+
+            this.player_name_box = new TextBox();
+            this.player_name_box.Location = new Point(300, 160);
+            this.player_name_box.Name = "Player_Name_Textbox";
+            this.player_name_box.Size = new System.Drawing.Size(100, 20);
+            this.player_name_box.TabIndex = 2;
+            //this.player_name_box.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.player_name_box);
+
+            this.player_name_label = new Label();
+            this.player_name_label.AutoSize = true;
+            this.player_name_label.Visible = true;
+            this.player_name_label.Location = new Point(200, 160);
+            this.player_name_label.Name = "Player_Name_Label";
+            this.player_name_label.Size = new System.Drawing.Size(79, 29);
+            this.player_name_label.TabIndex = 3;
+            this.player_name_label.Text = "Player Name:";
+
+            this.server_address_box = new TextBox();
+            this.server_address_box.Location = new Point(300, 206);
+            this.server_address_box.Name = "Server_Address_Textbox";
+            this.server_address_box.Size = new System.Drawing.Size(100, 20);
+            this.server_address_box.TabIndex = 2;
+            //this.server_address_box.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.server_address_box);
+
+            this.server_label = new Label();
+            this.server_label.AutoSize = true;
+            this.server_label.Visible = true;
+            this.server_label.Location = new Point(200, 206);
+            this.server_label.Name = "Server_Label";
+            this.server_label.Size = new System.Drawing.Size(79, 29);
+            this.server_label.TabIndex = 3;
+            this.server_label.Text = "Server:";
+
+            this.error_label = new Label();
+            this.error_label.AutoSize = true;
+            this.error_label.Visible = true;
+            this.error_label.Location = new Point(25, 400);
+            this.error_label.Name = "Error_Label";
+            this.error_label.Size = new System.Drawing.Size(79, 29);
+            this.error_label.TabIndex = 3;
+            this.error_label.Text = "Error: ";
+
+            this.title_label = new Label();
+            this.title_label.AutoSize = true;
+            this.title_label.Visible = true;
+            this.title_label.Location = new Point(287, 53);
+            this.title_label.Name = "Title_Label";
+            this.title_label.Size = new System.Drawing.Size(79, 29);
+            this.title_label.TabIndex = 3;
+            this.title_label.Text = "The Blob";
+            this.title_label.Font = new Font("Arial", 16);
+
+            this.Controls.Add(connect_button);
+            this.Controls.Add(player_name_box);
+            this.Controls.Add(player_name_label);
+            this.Controls.Add(server_address_box);
+            this.Controls.Add(server_label);
+            this.Controls.Add(error_label);
+            this.Controls.Add(title_label);
         }
 
         #endregion
 
-        private Button Connect_Button;
-
+        private Button connect_button;
+        private TextBox player_name_box;
+        private Label player_name_label;
+        private TextBox server_address_box;
+        private Label server_label;
+        private Label error_label;
+        private Label title_label;
 
     }
 }
